caa899719d3deb194af5164c5d5770a3fd5ed1c2
KenG98 <kengarber98@gmail.com>
2015-03-11 12:18:18 -0400

    Initial Commit

---
+++ Budget App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Budget App
+//
+//  Created by Ken Garber on 3/11/15.
+//  Copyright (c) 2015 Ken Garber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
