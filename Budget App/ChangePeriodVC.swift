f68110cd396572416782b29c458d87b36e0b3a23
cheses100 <cheses100@gmail.com>
2015-05-06 10:58:49 -0400

    Recreated Change Period
    Once again I hate constraints. Mostly got it working but the cancel
    button isn’t showing up

---
+++ Budget App/ChangePeriodVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChangePeriodVC.swift
+//  Budget App
+//
+//  Created by Daniel Mizrahi on 5/6/15.
+//  Copyright (c) 2015 Ken Garber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChangePeriodVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    @IBOutlet weak var periodPicker: UIPickerView!
+    
+    var options = ["Daily", "Weekly", "Monthly", "Yearly"]
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return options.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return options[row]
+    }
+}
