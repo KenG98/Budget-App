7a9b45741c9a7b87248fa4fb553a1f74e76e06e1
KenG98 <kengarber98@gmail.com>
2015-05-14 00:10:21 -0400

    Fixed merging error and added some test things to create a bar graph
    This stack overflow answer helped fix the merging error. No more
    “integrity” error. Yay!
    http://stackoverflow.com/questions/26472543/ios-git-merge-conflicts-afte
    r-adding-cocoapods-to-project

--- Budget App/StatsVC.swift
+++ Budget App/StatsVC.swift
@@ -6,4 +6,37 @@
 //  Copyright (c) 2015 Ken Garber. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StatsVC: UIViewController, JBBarChartViewDelegate, JBBarChartViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let barChartView = JBBarChartView();
+        barChartView.dataSource = self;
+        barChartView.delegate = self;
+        barChartView.backgroundColor = UIColor.darkGrayColor();
+        barChartView.frame = CGRectMake(0, 20, self.view.bounds.width, self.view.bounds.height * 0.5);
+        barChartView.reloadData();
+        self.view.addSubview(barChartView);
+        println("Launched");
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfBarsInBarChartView(barChartView: JBBarChartView!) -> UInt {
+        println("numberOfBarsInBarChartView");
+        return 10 //number of lines in chart
+    }
+    
+    func barChartView(barChartView: JBBarChartView, heightForBarViewAtIndex index: UInt) -> CGFloat {
+        println("barChartView", index);
+        
+        return CGFloat(arc4random_uniform(100));
+    }
+    
+}
---
+++ JBChartView-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  JBChartView-Bridging-Header.h
+//  Budget App
+//
+//  Created by Ken Garber on 5/13/15.
+//  Copyright (c) 2015 Ken Garber. All rights reserved.
+//
+
+#ifndef Budget_App_JBChartView_Bridging_Header_h
+#define Budget_App_JBChartView_Bridging_Header_h
+
+
+#endif
+
+#import <UIKit/UIKit.h>
+#import "JBChartView/JBChartView.h"
+#import "JBChartView/JBBarChartView.h"
+#import "JBChartView/JBLineChartView.h"
